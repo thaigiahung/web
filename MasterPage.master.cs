d486223e35d6e1f22d13362fb8d29c09431fdf26
Thai Gia Hung <hung.tg89@gmail.com>
2013-11-20 19:38:44 +0700

    Multiview

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -9,6 +9,12 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        MultiView1.ActiveViewIndex = (Session["User"] != null ? 1 : 0);
+        MultiView2.ActiveViewIndex = (Session["User"] != null ? 1 : 0);
+    }
+    protected void btnSignout_Click(object sender, EventArgs e)
+    {
+        Session.Remove("User");
+        Response.Redirect("login.aspx");
     }
 }

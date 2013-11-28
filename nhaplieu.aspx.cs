3e6db1b513cb61742b3274d439b73291fdc14b62
Daniel Huynh <kid.angel1412@gmail.com>
2013-11-28 09:58:51 +0700

    them du lieu cho shop

---
+++ nhaplieu.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+
+public partial class nhaplieu : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        WebGameDataContext db = new WebGameDataContext();
+        var q = from n in db.original_items
+                select n;
+        foreach (var w in q)
+        {
+            shop s = new shop();
+            s.shop_item_id = w.ID;
+            s.shop_item_price = 1000;
+            db.shops.InsertOnSubmit(s);
+        }
+        try
+        {
+            db.SubmitChanges();
+            lbTB.Text = "Done!";
+        }
+        catch
+        {
+            lbTB.ForeColor = Color.Maroon;
+            lbTB.Text = "Error!";
+        }
+    }
+}
